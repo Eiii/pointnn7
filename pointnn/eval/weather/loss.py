315f70aee612a48ad690f905c09740d8b9109605
Erich Merrill <erich@erichmerrill.com>
2022-05-08 00:24:52 -0700

    Updated weather loss

--- pointnn/eval/weather/loss.py
+++ pointnn/eval/weather/loss.py
@@ -1,34 +1,35 @@
-from ...data.weather import WeatherDataset, collate, collate_voxelize
+from ...data.weather import WeatherDataset, collate
 from ...problem.weather import flat_loss, scaled_loss
-
 from .. import common
-
-import math
-import functools
-import argparse
-import itertools
-from pathlib import Path
-
-import torch
 from torch.utils.data import DataLoader
-import torch.nn.functional as F
-
+import math
+import argparse
+import torch
 import pickle
+
 
 def seq_to_device(d, device):
     if isinstance(d, dict):
-        return {k:v.to(device) for k,v in d.items()}
+        return {k: v.to(device) for k, v in d.items()}
     elif isinstance(d, list):
         return [v.to(device) for v in d]
 
-def pred(model, ds, voxel=False, device='cpu'):
-    if voxel:
-        col = functools.partial(collate_voxelize, 6000)
-    else:
-        col = collate
+
+def pred_safe(model, ds, bs, device):
+    while bs != 1:
+        print(f'batch size={bs}')
+        try:
+            return pred(model, ds, bs, device)
+        except RuntimeError as e:
+            print(e)
+            bs = bs // 2
+    raise RuntimeError()
+
+
+def pred(model, ds, bs, device):
     losses = []
     scaled_losses = []
-    loader = DataLoader(ds, batch_size=64, collate_fn=col, num_workers=8)
+    loader = DataLoader(ds, batch_size=bs, collate_fn=collate, num_workers=4)
     for item in loader:
         item = seq_to_device(item, device)
         args = model.get_args(item)
@@ -43,11 +44,13 @@ def pred(model, ds, voxel=False, device='cpu'):
         scaled_losses.append(sls)
     return torch.cat(losses), torch.cat(scaled_losses)
 
-def run_net(net, ds, voxel):
+
+def run_net(net, ds, bs):
     with torch.no_grad():
         net = net.eval().cuda()
         #ens = SelfEnsemble(net, 3, {'force_dropout': 0.2})
-        return pred(net, ds, voxel, device='cuda')
+        return pred_safe(net, ds, bs, device='cuda')
+
 
 def show_results(losses):
     mean_preds = losses.mean(dim=0)
@@ -56,36 +59,19 @@ def show_results(losses):
     print(mean.tolist())
     return mean
 
-def main(data_path, net_paths, drop):
-    ds = WeatherDataset(data_path, load_train=False, drop=drop, HACK_SNIP=1000)
-    ds = ds.test_dataset
-    print(len(ds))
-    for net_path in net_paths:
-        result = common.load_result(net_path)
-        net = common.make_net(result)
-        show_results(run_net(net, ds))
 
-def batch(data_path, path, drop):
-    names = ['TPC', 'SeFT', 'DeepSets', 'Mink']
+def batch(data_path, net_paths, bs, drop, out_path):
     ds = WeatherDataset(data_path, load_train=False, drop=drop)
     ds = ds.test_dataset
     loss_dict = {}
-    for name in names:
-        print(name)
-        net_paths = path.glob(name+'*.pkl')
-        res_l = []
-        scaled_l = []
-        for net_path in net_paths:
-            print('x')
-            net = common.make_net(common.load_result(net_path))
-            losses, scaled_loss = run_net(net, ds, voxel=(name=='Mink'))
-            res_l.append(losses)
-            scaled_l.append(scaled_loss)
-        all_losses = torch.cat(res_l)
-        all_scaled_losses = torch.cat(scaled_l)
-        loss_dict[name] = (all_losses.cpu(), all_scaled_losses.cpu())
-    with open('weather-loss.pkl', 'wb') as fd:
-        pickle.dump(loss_dict, fd)
+    for net_path in net_paths:
+        print(net_path)
+        net = common.make_net(common.load_result(net_path))
+        loss, scaled_loss = run_net(net, ds, bs)
+        loss_dict[net_path] = {'loss': scaled_loss, 'mse': loss}
+        with open(out_path, 'wb') as fd:
+            pickle.dump(loss_dict, fd)
+
 
 def table(path):
     with open(path, 'rb') as fd:
@@ -111,15 +97,13 @@ def table(path):
 def make_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument('--data', default='./data/weather')
-    parser.add_argument('--net', default=Path('./output/weather'), type=Path)
+    parser.add_argument('--nets', nargs='+')
+    parser.add_argument('--out', default='weather-loss.pkl')
     parser.add_argument('--drop', default=0, type=float)
+    parser.add_argument('--bs', default=32, type=int)
     parser.add_argument('--table', action='store_true')
     return parser
 
 if __name__=='__main__':
     args = make_parser().parse_args()
-    #main(args.data, args.net, args.drop)
-    if args.table:
-        table(args.data)
-    else:
-        batch(args.data, args.net, args.drop)
+    batch(args.data, args.nets, args.bs, args.drop, args.out)
