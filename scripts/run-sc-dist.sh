719ff991f8b223d70e0baf43eb2a1ce5ed70de27
Erich Merrill <erich@erichmerrill.com>
2022-05-09 20:52:55 -0700

    Updated sc2 dist script

--- scripts/run-sc-dist.sh
+++ scripts/run-sc-dist.sh
@@ -4,7 +4,10 @@ DATA=/nfs/stak/users/merriler/cluster/hpc-share/data/sc2scene/test
 NETS=/nfs/stak/users/merriler/cluster/hpc-share/output/t-sc-final
 OUT=/nfs/stak/users/merriler/cluster/hpc-share/output
 
-TPCS=$NETS/TPC-Med*
-TS=$(seq -s , 10)
+PREDS="$NETS/*OnePred* $NETS/*TwoPred*"
+BASE="$NETS/TPC-Med\:9* $NETS/TInt-Med\:0* $NETS/TGC-Med\:5*"
 
-python -m pointnn.eval.sc2.loss --data $DATA --bs 256 --net $TPCS --out $OUT/dist-sc2-tpc.pkl --ts $TS
+for t in $(seq 12) ; do
+  echo $t
+  python -m pointnn.eval.sc2.loss --data $DATA --bs 150 --net $PREDS $BASE --out $OUT/sc2-tpc-dist-${t}.pkl --t $t
+done
