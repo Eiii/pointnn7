4db1b475ad48dd28669060ba71745a722b35705e
Erich Merrill <erich@erichmerrill.com>
2022-05-10 11:36:06 -0700

    Updated weather ensemble script

--- scripts/we-ensemble.sh
+++ scripts/we-ensemble.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
 
 DATA=/nfs/stak/users/merriler/cluster/hpc-share/data/weather
-NETS=/nfs/stak/users/merriler/cluster/hpc-share/output/t-we-final/TPC-Med\:*
 OUT=/nfs/stak/users/merriler/cluster/hpc-share/output/
+TGTS="TPC-Med TGC-Med TInt-Med"  
+BASE=/nfs/stak/users/merriler/cluster/hpc-share/output/t-we-final
+NETS=""
+
+for tgt in $TGTS ; do
+  N=$(ls ${BASE}/${tgt}\:* | head -1)
+  D=$(ls ${BASE}/${tgt}-Drop2\:* | head -1)
+  NETS="${NETS} $N $D"
+done
+
+echo $NETS
 
 for d in 0.1 0.2 0.5 ; do
   echo $d
