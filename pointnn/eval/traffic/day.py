48e5e8cbb1258b1b8ec0ee59250a1fe08069bec6
Erich Merrill <erich@erichmerrill.com>
2022-05-09 23:44:53 -0700

    Traffic day eval

--- pointnn/eval/traffic/day.py
+++ pointnn/eval/traffic/day.py
@@ -1,29 +1,32 @@
 from ...data.traffic import METRDataset, collate
-from ...nets.traffic.baseline import Random, Nearest, Mean
+from ...nets.traffic.baseline import Nearest
 from .. import common
 import matplotlib
-#matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import matplotlib.colors as colors
-from matplotlib.cm import ScalarMappable
-from matplotlib import collections as coll
-from matplotlib.animation import ArtistAnimation
 
 from pathlib import Path
 import argparse
 import torch
 
+
 def to_cuda(d):
     return {k:v.cuda() if v is not None else None for k,v in d.items()}
+
 
 def make_parser():
     def_data = './data/traffic/METR-LA/'
     parser = argparse.ArgumentParser()
     parser.add_argument('--data', default=Path(def_data))
+    parser.add_argument('--out', type=Path, default=Path('day'))
     parser.add_argument('--net', nargs='+')
-    parser.add_argument('--normalize', action='store_true')
-    parser.add_argument('--spectral', action='store_true')
+    parser.add_argument('--month', type=int)
+    parser.add_argument('--day', type=int)
+    parser.add_argument('--sensors', nargs='+', type=int)
+    parser.add_argument('--xstart', type=int)
+    parser.add_argument('--xend', type=int)
     return parser
+
 
 def plot_traffic(ax, ds, sensor, norm_info):
     first_x = None
@@ -31,7 +34,7 @@ def plot_traffic(ax, ds, sensor, norm_info):
     xs = []
     ys = []
     for entry in ds.entries:
-        ref = entry.center
+        ref = entry.target_rows[0]
         first_x = first_x or ref
         time = ds.raw_times[ref]
         col = ds.raw_df.loc[ref]
@@ -42,6 +45,7 @@ def plot_traffic(ax, ds, sensor, norm_info):
         xs.append(ref-first_x)
         ys.append(val)
     ax.plot(xs, ys, label='actual', c='k')
+
 
 def plot_preds(ax, ds, preds, sensor_id, norm_info, name='pred'):
     mean, std = norm_info
@@ -56,6 +60,7 @@ def plot_preds(ax, ds, preds, sensor_id, norm_info, name='pred'):
     ax.plot(xs, ys, label=name)
     ax.set_ylim([0, 70])
 
+
 @torch.no_grad()
 def predict(net, ds):
     preds = []
@@ -67,33 +72,32 @@ def predict(net, ds):
         preds.append(pred)
     return preds
 
+
 def make_net(path):
     net = common.make_net(common.load_result(path))
     net = net.cuda().eval()
     return net
 
-def make_baseline(type_):
-    if type_ == 'random':
-        return Random([0, 70])
-    elif type_ == 'nearest':
-        return Nearest()
-    elif type_ == 'mean':
-        return Mean()
 
-def single(base, net_paths, norm, spec):
-    valid_fn = lambda date: date.week % 10 == 0 and date.month == 5 and date.day == 20
-    dims = 50
-    ds = METRDataset(base, valid_fn, norm, spec, dims)
-    norm_info = ds.norm_info if norm else [0, 1]
+def get_net_type(net_path):
+    start = net_path.rfind('/')+1
+    end = net_path.index(':')
+    return net_path[start:end]
+
+
+def single(base, net_paths, month, day, out, sensors, xstart, xend):
+    filter_fn = lambda date: date.week % 10 == 0 and date.month == month and date.day == day
+    ds = METRDataset(base, filter_fn, True, False)
+    assert len(ds) == 288, "Date isn't in the test dataset"
+    norm_info = ds.norm_info
     all_ids = ds.raw_df.columns.tolist()
-    assert len(ds) == 288
     models = [make_net(path) for path in net_paths]
     preds = [predict(model, ds) for model in models]
-    names = ['TPC', 'Spectral', 'Blank']
-    mean_model = make_baseline('mean')
-    mean_preds = predict(mean_model, ds)
-    for sensor_id in all_ids:
-        fig, ax = plt.subplots(figsize=(10,10), dpi=100)
+    names = [get_net_type(p) for p in net_paths]
+    base_model = Nearest()
+    mean_preds = predict(base_model, ds)
+    for sensor_id in sensors if sensors else all_ids:
+        fig, ax = plt.subplots(figsize=(10, 10), dpi=100)
         print(sensor_id)
         plot_traffic(ax, ds, sensor_id, norm_info)
         sensor_idx = ds.sensor_id_to_idx[int(sensor_id)]
@@ -104,6 +108,9 @@ def single(base, net_paths, norm, spec):
         fig.savefig(f'day/{sensor_id}.png')
         plt.close(fig)
 
+
 if __name__ == '__main__':
     args = make_parser().parse_args()
-    single(args.data, args.net, args.normalize, args.spectral)
+    if not args.out.exists():
+        args.out.mkdir()
+    single(args.data, args.net, args.month, args.day, args.out, args.sensors, args.xstart, args.xend)
