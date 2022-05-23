86b3b1fda06495883eace35af060534754ef6fc2
Erich Merrill <erich@erichmerrill.com>
2022-05-22 20:35:18 -0700

    Dumb script updates

--- scripts/run-sc-dist.sh
+++ scripts/run-sc-dist.sh
@@ -7,6 +7,9 @@ OUT=/nfs/stak/users/merriler/cluster/hpc-share/output
 PREDS="$NETS/*OnePred* $NETS/*TwoPred*"
 BASE="$NETS/TPC-Med\:9* $NETS/TInt-Med\:0* $NETS/TGC-Med\:5*"
 
+echo $PREDS
+echo $BASE
+
 for t in $(seq 12) ; do
   echo $t
   python -m pointnn.eval.sc2.loss --data $DATA --bs 150 --net $PREDS $BASE --out $OUT/sc2-tpc-dist-${t}.pkl --t $t
