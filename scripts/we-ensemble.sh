cf35afea234dc36d57400f4f2d630ed948c1e2e2
Erich Merrill <erich@erichmerrill.com>
2022-05-22 21:17:50 -0700

    Updated we eval script

--- scripts/we-ensemble.sh
+++ scripts/we-ensemble.sh
@@ -14,7 +14,7 @@ done
 
 echo $NETS
 
-for d in 0.1 0.2 0.5 ; do
+for d in 0 0.1 0.2 0.5 ; do
   echo $d
   python -m pointnn.eval.weather.loss --data $DATA --bs 32 --net $NETS --out $OUT/weather-drop-${d/./}.pkl --drop $d
 done
