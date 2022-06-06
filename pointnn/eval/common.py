bbeabb7f1257aaee235bc6d874ef64127989079f
Erich Merrill <erich@erichmerrill.com>
2022-06-05 22:39:47 -0700

    eval common

--- pointnn/eval/common.py
+++ pointnn/eval/common.py
@@ -1,8 +1,7 @@
-from ..data.loader import ModelnetDataset
+from .. import nets
+
 import pickle
 from pathlib import Path
-import torch
-from .. import nets
 
 
 def load_any(in_path):
@@ -27,38 +26,3 @@ def make_net(result):
     net = nets.make_net_args(nt, na)
     net.load_state_dict(result['state_dict'])
     return net
-
-
-def all_net_names(results):
-    return {m['measure'].name for m in results}
-
-
-def nets_by_name(results, name):
-    return [(m, n) for m, n in results if m['measure'].name == name]
-
-
-def filter_best(results):
-    bests = {}
-    get_last_valid = lambda x: x['measure']._valid_stats[-1][2]
-    for r in results:
-        name = r['measure'].name
-        if name not in bests or get_last_valid(r) < get_last_valid(bests[name]):
-            bests[name] = r
-    return list(bests.values())
-
-
-def filter_name(results, name):
-    if not name:
-        return results
-    return [r for r in results if name in m['measure'].name]
-
-
-def get_dataset_classes(path10, path40=None):
-    ds = ModelnetDataset(path10, type='test')
-    classes10 = list(ds.classes)
-    if path40 is not None:
-        ds = ModelnetDataset(path40, type='test')
-        classes40 = [x for x in ds.classes if x not in classes10]
-        return classes10, classes40
-    else:
-        return classes10
