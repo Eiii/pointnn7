9e5ac327709bf6461fa4692536e46f1291ee4aed
Erich Merrill <erich@erichmerrill.com>
2022-06-06 00:32:21 -0700

    eval weather map

--- pointnn/eval/weather/map.py
+++ pointnn/eval/weather/map.py
@@ -1,21 +1,22 @@
+import itertools
 import argparse
+
+import numpy as np
+
 from . import common
+from ...data.weather import WeatherDataset
 
 import matplotlib
 matplotlib.use('Agg')
 from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-import numpy as np
-import itertools
-
-from ...data.weather import WeatherDataset
-
 
 def get_stations(ds):
     pos = ds.raw_metadata[['x_m', 'y_m']]
     names = list(ds.raw_metadata.index)
     return names, pos.to_numpy()
+
 
 COLORS = ['r', 'g', 'yellow', 'b']
 def plot_loc(ax, names, pts, show_names=True, highlight_groups=None):
@@ -28,17 +29,14 @@ def plot_loc(ax, names, pts, show_names=True, highlight_groups=None):
             xy = zip(*group_pts)
             ax.scatter(*xy, c=color)
     if show_names:
-        for name, pt in zip(names, pts): ax.annotate(name, pt)
+        for name, pt in zip(names, pts):
+            ax.annotate(name, pt)
     # Set up ax
     ax.set_xticks([])
     ax.set_yticks([])
     ax.set_xlabel("X")
     ax.set_ylabel("Y", rotation=0, labelpad=20)
 
-def make_parser():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--data', default='./data/weather')
-    return parser
 
 def sample_space_groups(pts, num_groups=1, group_size=12):
     p1 = np.expand_dims(pts, 0)
@@ -49,6 +47,7 @@ def sample_space_groups(pts, num_groups=1, group_size=12):
     closest = closest[:num_groups]
     closest = closest[:, :group_size]
     return closest
+
 
 def make_3d(pts, timesteps=3, dist=1):
     ids = np.arange(pts.shape[0])
@@ -62,6 +61,7 @@ def make_3d(pts, timesteps=3, dist=1):
     ids = ids.reshape(-1)
     return pts, ids
 
+
 def plot_time(ax, pts, ids, num_groups=4):
     ax.view_init(elev=10, azim=-65)
     # Choose IDs to display
@@ -74,8 +74,8 @@ def plot_time(ax, pts, ids, num_groups=4):
     ax.scatter(ts, xs, ys, depthshade=False, c='black', alpha=0.8, s=10,
                edgecolors='none')
     for i, color in zip(grp_ids, COLORS):
-        grp_pts = pts[ids==i]
-        xs, ys,ts = zip(*grp_pts)
+        grp_pts = pts[ids == i]
+        xs, ys, ts = zip(*grp_pts)
         ax.scatter(ts, xs, ys, depthshade=False, c=color, s=40,
                    edgecolors='none')
         ax.plot(ts, xs, ys, c=color)
@@ -90,11 +90,7 @@ def plot_time(ax, pts, ids, num_groups=4):
     ax.yaxis.pane.fill = False
     ax.zaxis.pane.fill = True
     ax.grid(True)
-    """
-    ax.xaxis.pane.set_edgecolor('black')
-    ax.yaxis.pane.set_edgecolor('black')
-    ax.zaxis.pane.set_edgecolor('black')
-    """
+
 
 def main(path):
     common.config_mpl()
@@ -115,6 +111,12 @@ def main(path):
     plt.savefig('map_time.png')
 
 
-if __name__=='__main__':
+def make_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data', default='./data/weather')
+    return parser
+
+
+if __name__ == '__main__':
     args = make_parser().parse_args()
     main(args.data)
